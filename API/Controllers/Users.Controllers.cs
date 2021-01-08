7f15b057c40215de333f660f1ebf51430cef735d
gretchunkim <gretchunkim@gmail.com>
2021-01-07 23:28:38 -0500

    commit to main

--- API/Controllers/Users.Controllers.cs
+++ API/Controllers/Users.Controllers.cs
@@ -25,9 +25,10 @@ namespace API.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id)
+        public ActionResult<AppUser> GetUser(int id)
         {
-            return await _context.Users.FindAsync(id);
+            return _context.Users.Find(id);
+
         }
     }
 }
